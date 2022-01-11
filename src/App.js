83a090e5669e9d866d013d8ea72b2ee0bc83dc58
swethav08711 <shwe08711@gmail.com>
2022-01-11 15:25:09 +0530

    detail

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Product from "./component/Product/Product"
+import ProductDetail from "./component/productDetail/ProductDetail"
 
 function App() {
   return (
     <div>
       <Product />
+      <ProductDetail />
     </div>
   )
 }
