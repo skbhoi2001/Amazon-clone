8befa09a552032b0c6be9514e9021096d84fd09b
swethav08711 <shwe08711@gmail.com>
2022-01-14 12:36:25 +0530

    cart pages

--- src/components/productPage/checkoutPage/placedSuccessPage/PlacedSuccessPage.jsx
+++ src/components/productPage/checkoutPage/placedSuccessPage/PlacedSuccessPage.jsx
@@ -1,7 +1,55 @@
 import React from "react"
-
+import NavBar from "../../../navBar/NavBar"
+import "./placedSucccess.css"
 const PlacedSuccessPage = () => {
-  return <div>hiiiiiiiiiiii</div>
+  return (
+    <>
+      <NavBar />
+      <div className="successPage_main">
+        <div className="successPage_main_container">
+          <div>
+            <h3 className="green_heading">Order placed, thank you!</h3>
+            <p>Confirmation will be sent to your email.</p>
+
+            <h4 className="success_aligningitems">
+              Shipping to{" "}
+              <span>
+                <h4>Swetha V,</h4>
+              </span>
+              #25
+              <span>
+                <h4>
+                  Basaveshvar layoput , kadabgere cross, BENGALURU ,
+                  KARNATAKE,562130
+                </h4>
+              </span>{" "}
+            </h4>
+            <hr />
+            <div className="success_align_flexing_items">
+              <div className="success_align_flexing_items_padding">
+                <h4 className="Color_green">Thursday, 20 Jan</h4>
+                <p className="Pading12">Delivery date</p>
+              </div>
+              <div className="displaying_flecxing_imagwes">
+                <img
+                  className="img_success_widthing"
+                  src="https://m.media-amazon.com/images/I/61KuMWWVwLL._AC_AA160_.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              className="img_success_page"
+              src="https://m.media-amazon.com/images/G/01/aa/ich/TYPIMAGE2._CB669772659_.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default PlacedSuccessPage
--- src/components/productPage/checkoutPage/reviewPage/ReviewPage.jsx
+++ src/components/productPage/checkoutPage/reviewPage/ReviewPage.jsx
@@ -1,12 +1,95 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-
+import "./reviewpage.css"
 const ReviewPage = () => {
   return (
-    <div>
-      <button>
-        <NavLink to="/placed">Place your order</NavLink>
-      </button>
+    <div className="buynow_main ">
+      <nav>
+        <img
+          className="buynow_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/x-locale/checkout/confirm-banner._CB485949149_.gif"
+          alt="gif"
+        />
+      </nav>
+      <div className="buynow_main2">
+        <h1 className="checout_headin">Review your order</h1>
+        <p>
+          By placing your order, you agree to Amazon's{" "}
+          <span>privacy notice</span> and <span>conditions of use</span>{" "}
+        </p>
+        <div className="review_detail_maina">
+          <div className="review_detail_add">
+            <h3 className="review_h3">Shipping address</h3>
+            <p className="review_p">Swetha V</p>
+            <p className="review_p">#15</p>
+            <p className="review_p">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repellendus perferendis
+            </p>
+            <p className="review_p">india</p>
+            <p className="review_p">Phone : 7892839931</p>
+            <p className="review_pAdd">Add delivery instructions</p>
+          </div>
+          <div className="review_detail_add">
+            <h3 className="review_h3">Payment method</h3>
+            <p className="review_p">Pay on delivery (Cash/Card)</p>
+          </div>
+          <div className="review_detail_add">
+            <h3 className="review_h3">Gift card, Voucher</h3>
+            <input type="number" placeholder="Enter Code" />
+            <button>Apply</button>
+          </div>
+        </div>
+        <div className="review_detail_mainq">
+          <h2>Delivery date:20 Jan 2022</h2>
+          <div className="review_detail_main">
+            <img
+              className="review_detail_main_img"
+              src="https://images-eu.ssl-images-amazon.com/images/I/61KuMWWVwLL._AC_AA150_.jpg"
+              alt=""
+            />
+            <div className="review_main_cass">
+              <p className="review_desc">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi,
+                ad. Ex
+              </p>
+              <div className="review_desc_price_tag">
+                <p className="padding_p_tag1">₹16999</p>
+                <p className="padding_p1_tag2">₹598</p>
+                <img
+                  className="width_image padding_p_tag"
+                  src="https://images-eu.ssl-images-amazon.com/images/G/31/marketing/fba/fba-badge_18px._CB485936079_.png"
+                  alt=""
+                />
+              </div>
+              <p className="padding_quantity_selected">Quantity: 1</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="OrderSummary_page">
+        <NavLink to="/placed">
+          <button className="OrderSummary_page_button">Place your order</button>
+        </NavLink>
+        <h3 className="OrderSummary_page_h3">Order Summary</h3>
+        <div className="OrderSummary_page_proice">
+          <div>
+            <p>Items:</p>
+            <p>Delivery:</p>
+            <p>Total:</p>
+          </div>
+          <div>
+            <p>₹597</p>
+            <p>₹40</p>
+            <p>₹637</p>
+          </div>
+        </div>
+        <hr />
+        <div className="OrderSummary_page_proice ">
+          <p className="OrderSummary_page_Red_color">Order Total:</p>
+          <p className="OrderSummary_page_Red_color">₹597</p>
+        </div>
+      </div>
     </div>
   )
 }
