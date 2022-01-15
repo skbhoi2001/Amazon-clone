4f91039fabf1ebc68b278f06180bcd9335dc2d66
akashdwivedi6261 <adarshsen6261@gmail.com>
2022-01-15 14:56:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 import Signup from './Components/Authentication/Signup.jsx'
 import Login from './Components/Authentication/Login.jsx'
+import Footer from './Components/Authentication/Footer'
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Footer />
     </div>
   );
 }
